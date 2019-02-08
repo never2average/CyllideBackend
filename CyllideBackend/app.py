2008d36ddfd07fc1adf1649e30fee8482ec1df48
never2average <priyesh.sriv2017@gmail.com>
2019-02-08 15:54:03 +0300

    Test added

--- CyllideBackend/app.py
+++ CyllideBackend/app.py
@@ -1,17 +1,22 @@
-from flask import Flask,jsonify,make_response,render_template,redirect,url_for,Markup
-from flask_restful import Resource,Api,request
+from flask import Flask, jsonify, make_response
+from flask_restful import Resource, Api, request
 from yahoo_fin.stock_info import get_quote_table
 from keys import secretKey
 import json
 
-app=Flask(__name__)
-api=Api(app)
+
+app = Flask(__name__)
+api = Api(app)
+
 
 class TestConnection(Resource):
+    def get(self):
+        return jsonify({
+            "message": "APIS working"
+            })
 
-api.add_resource()
+api.add_resource("/testconn", TestConnection)
 
 
-
-if __name__=="__main__":
-    app.run(port=5000,debug=True)
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
