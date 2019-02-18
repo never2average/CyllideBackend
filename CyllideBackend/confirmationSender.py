0dbd61b4f220c93ce844ec2e72e785de0722f2a3
Shivam Gera <Shivamgera@github.com>
2019-02-19 03:03:09 +0530

    [WIP] Implement sms authentication

---
+++ CyllideBackend/confirmationSender.py
@@ -0,0 +1,26 @@
+from twilio.rest import Client
+from flask import session
+import random
+ 
+TWILIO_ACCOUNT_SID = 'ACb78228e61fc41d231e024ee98aba08f9'
+TWILIO_AUTH_TOKEN = '49b3c94dc970d1a52d2444d6f30c1f22'
+TWILIO_NUMBER = +12014313496
+def send_confirmation_code(to_number):
+    verification_code = generate_code()
+    send_sms(to_number, verification_code)
+    session['verification_code'] = verification_code
+    return verification_code
+
+
+def generate_code():
+    return str(random.randrange(100000, 999999))
+
+
+def send_sms(to_number, body):
+    account_sid = TWILIO_ACCOUNT_SID
+    auth_token = TWILIO_AUTH_TOKEN
+    twilio_number = TWILIO_NUMBER
+    client = Client(account_sid, auth_token)
+    client.api.messages.create(to_number,
+                           from_=twilio_number,
+                           body=body)
