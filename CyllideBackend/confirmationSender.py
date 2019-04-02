d238492a1c4bb4c259117e872d6fbd61997a091d
never2average <priyesh.sriv2017@gmail.com>
2019-04-03 02:14:26 +0530

    added profile info

--- CyllideBackend/confirmationSender.py
+++ CyllideBackend/confirmationSender.py
@@ -140,15 +140,18 @@ def getProfileInfoOthers(token, username):
 
 
 def sendFeedback(token, text):
-    port = 465
-    smtp_server = "smtp.gmail.com"
-    sender_email = "batchjobrocks@gmail.com"
-    receiver_email = "prasannkumar1263@gmail.com"
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-        server.login(sender_email, "Password##123")
-        server.sendmail(sender_email, receiver_email, text)
-
+    try:
+        port = 465
+        smtp_server = "smtp.gmail.com"
+        sender_email = "batchjobrocks@gmail.com"
+        receiver_email = "prasannkumar1263@gmail.com"
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+            server.login(sender_email, "Password##123")
+            server.sendmail(sender_email, receiver_email, text)
+        return json.loads({"data": "Email sent successfully"}), 200
+    except:
+        return json.loads({"data": "Email sending failed"}), 401
 
 def validateToken(token):
     try:
