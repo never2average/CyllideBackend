c1351354457197d10d4e77ba0fea4fadc3661b45
never2average <priyesh.sriv2017@gmail.com>
2019-04-12 05:00:52 +0530

    delete position working

--- CyllideBackend/portfolioConnectors.py
+++ CyllideBackend/portfolioConnectors.py
@@ -91,8 +91,8 @@ def deletePosition(token, portfolioID, state, ticker, quantity, isLong):
                     ll.append(Positions(
                         ticker=ll[i].ticker,
                         quantity=ll[i].quantity,
-                        longPosition=not isLong),
-                        entryTime=datetime.now()
+                        longPosition=not isLong,
+                        entryTime=datetime.now())
                     )
                     ll.pop(i)
                     break
