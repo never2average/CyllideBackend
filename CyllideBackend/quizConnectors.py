b5b4dae2397023843702604020fd490ae7197f6b
never2average <priyesh.sriv2017@gmail.com>
2019-05-30 16:24:56 +0530

    So so minutes

--- CyllideBackend/quizConnectors.py
+++ CyllideBackend/quizConnectors.py
@@ -185,7 +185,7 @@ if __name__ == "__main__":
     # for i in range(23):
     #     for j in range(0, 60, 5):
     quizData = {
-        "start_date": "May 30 2019 18:00",
+        "start_date": "May 30 2019 19:00",
         "prize_money": 0,
         "questions":
         [
