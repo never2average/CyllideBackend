eb5f5c0b633d3e58b58c059fdf50177bd498aaec
never2average <priyesh.sriv2017@gmail.com>
2019-05-30 15:26:49 +0530

    Added QuizID Input

--- CyllideBackend/adminConnectors.py
+++ CyllideBackend/adminConnectors.py
@@ -110,7 +110,9 @@ def addQuiz(token, data):
             )
         )
         os.system(
-            'aws events put-targets --rule QuizRemoteController --targets "Id"="1","Arn"="arn:aws:lambda:ap-south-1:588187310904:function:QuizRemoteControlLambda","Input"={}'.format(json.dumps({"qid":str(newQuiz.id)}))
+            'aws events put-targets --rule QuizRemoteController_{}_{}_{}_{} --targets "Id"="1","Arn"="arn:aws:lambda:ap-south-1:588187310904:function:QuizRemoteControlLambda","Input"={}'.format(
+                dobj.hour, dobj.day, dobj.month, dobj.year,json.dumps({"qid":str(newQuiz.id)})
+            )
         )
         return {
             "message": "QuizAddedSuccessfully",
