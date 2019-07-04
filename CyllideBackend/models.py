8f2fca0ee4ab1db9f64a9f55f0b93698c8817d26
never2average <priyesh.sriv2017@gmail.com>
2019-07-04 20:24:52 +0530

    Changed OTP

--- CyllideBackend/models.py
+++ CyllideBackend/models.py
@@ -177,7 +177,7 @@ class Query(Document):
 
 class TempAcc(Document):
     toNumber = IntField(required=True)
-    otp = IntField(required=True, min_value=100000, max_value=999999)
+    otp = IntField(required=True, min_value=1000, max_value=9999)
     username = StringField(required=True)
 
 
